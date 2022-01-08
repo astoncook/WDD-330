dc922d6ba0f47a0cfb4fb874f29bcdcabaa861ff
astoncook <coo19025@byui.edu>
2022-01-07 20:21:19 -0600

    first week commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const links = [
+    {
+      label: "Week 1 Notes",
+      url: "week1.html"
+    }
+  ]
+
+// Create a new list item for each link
+const ol = document.getElementById('assignments'); 
+
+// Loop through the links array
+links.forEach(link => {
+  let li = document.createElement('li');
+  let a = document.createElement('a');
+  
+// Set the text of the anchor tag to the label of the link
+  a.setAttribute('href', link.url);
+  a.innerText = link.label;
+
+// Append the anchor tag to the li 
+  li.appendChild(a);
+  ol.appendChild(li);
+
+});
