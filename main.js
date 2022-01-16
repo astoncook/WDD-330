20d65d56470c060d3c6075ede28ca82364019c30
astoncook <coo19025@byui.edu>
2022-01-15 19:39:48 -0600

    week 2 notes

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-const links = [
-    {
-      label: "Week 1 Notes",
-      url: "week1.html"
-    }
-  ]
+const links = [{
+    label: 'Week 1 Notes',
+    url: 'week1.html',
+  }, // you can add other tags if you like 
+  {
+    label: 'Week 2 Notes',
+    url: 'week2.html',
+  }
+];
 
 // Create a new list item for each link
 const ol = document.getElementById('assignments'); 
